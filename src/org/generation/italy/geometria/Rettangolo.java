0c83464e70521dfaa7543f52aa205e933c8f68cc
IlGrandeLuca <lucailgrande96@outlook.it>
2021-11-23 15:48:21 +0100

    Class Rettangolo added

---
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -0,0 +1,21 @@
+package org.generation.italy.geometria;
+
+public class Rettangolo {
+	// Attributes
+	int base;
+	int height;
+
+	// Methods
+	Rettangolo(int base, int height) {
+		this.base = base;
+		this.height = height;
+	}
+
+	int AreaCalculator() {
+		return base * height;
+	}
+
+	int perimeterCalculator() {
+		return (base * 2) + (height * 2);
+	}
+}
